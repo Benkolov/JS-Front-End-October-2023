0009743ea9abe02b1c8adde6ffca248442e08d8f
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-11-13 12:17:27 +0200

    Changes

--- 02.2. Exercise JS Arrays & Strings/07. stringSubstring.js
+++ 02.2. Exercise JS Arrays & Strings/07. stringSubstring.js
@@ -1,13 +1,17 @@
 function findWord(word, text) {
 
-    const lowercasedWord = word.toLowerCase();
-    const lowercasedText = text.toLowerCase();
+    let lowercasedText = text.toLowerCase().split(' ');
+    let output = `${word} not found!`;
 
-    if (lowercasedText.includes(lowercasedWord)) {
-        console.log(word);
-    } else {
-        console.log(`${word} not found!`);
+    for(let index = 0; index < lowercasedText.length; index++) {
+        let currentWord = lowercasedText[index];
+
+        if(currentWord === word) {
+            output = currentWord;
+        }
     }
+
+    console.log(output);
 }
 
 findWord('javascript', 'JavaScript is the best programming language');
