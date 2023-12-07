8a78816beb5df9b189503a1aef4e6dce8f2bef7f
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-12-07 17:48:39 +0200

    Add

--- 05.1. DOM Introduction/01.Edit Element/EditElement.js
+++ 05.1. DOM Introduction/01.Edit Element/EditElement.js
@@ -1,11 +1,7 @@
 function editElement(element, match, replacer) {
-    if (element instanceof Element) {
-        const textContentArray = Array.from(element.childNodes).filter(node => node.nodeType === Node.TEXT_NODE);
+    const content = element.textContent
+    const editContent = content.replace(new RegExp(match, 'g'), replacer);
+    element.textContent = editContent;
 
-        textContentArray.forEach(node => {
-            node.nodeValue = node.nodeValue.replace(new RegExp(match, 'g'), replacer);
-        });
-    } else {
-        console.error('Invalid element provided.');
-    }
+    console.log(editContent);
 }
