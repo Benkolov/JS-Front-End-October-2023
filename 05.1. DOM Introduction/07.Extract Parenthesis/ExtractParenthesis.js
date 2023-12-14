8b17b971eca784e3a8102ca29fc5c66a22ca4b70
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-12-14 20:19:58 +0200

    Add

--- 05.1. DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
+++ 05.1. DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,14 @@
-function extract(content) {
+function extract(id) {
+    const text = document.getElementById(id).textContent;
+    const pattern = /\((.+?)\)/g;
+    let result = [];
 
+    let match = pattern.exec(text);
+
+    while (match !== null) {
+        result.push(match[1]);
+        match = pattern.exec(text);
+    }
+
+    return result.join('; ');
 }
--- 06.1. DOM Manipulation and Events/01. List-Of-Items/app.js
+++ 06.1. DOM Manipulation and Events/01. List-Of-Items/app.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    const inputElement = document.querySelector('#newItemText');
+
+    const newListItem = document.createElement('li');
+    newListItem.textContent = inputElement.value;
+
+    const ulList = document.querySelector('#items');
+    ulList.appendChild(newListItem);
+
+    console.log(inputElement.value);
 }
